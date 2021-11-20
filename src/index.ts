6ff2f37a4bdea4ac6c2f3a86917a53ca261cd851
John Wells <molekewl2017@gmail.com>
2021-11-19 19:58:24 -0800

    setup express & graphql

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
-import microConfig from './mikro-orm.config';
+// import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./reolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: "first post" });
+  const app = express();
 
-  await orm.em.persistAndFlush(post);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts)
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server is running");
+  });
+  // const post = orm.em.create(Post, { title: "first post" });
+
+  // await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts)
 };
 
-main().catch(err => console.log(err))
+main().catch((err) => console.log(err));
